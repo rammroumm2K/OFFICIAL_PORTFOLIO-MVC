f8ab7fdda252506d7388b90a85ae2c7128fd50d9
bimmer <127702177+rammroumm2K@users.noreply.github.com>
2024-05-22 13:58:39 +0200

    by improving, we perfect ourselves
    👌

---
+++ view/error404View.php
@@ -0,0 +1,19 @@
+<?php
+ header("HTTP/1.0 404 Not Found");
+?>
+<html>
+    <body style="background-color: red;">
+        
+    
+    <head>
+        <title><?=$title?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <center>
+                <h1 style="color: white;"  >404 Not Found</h1>
+                <p style="color: white;"> The page you are looking for does not exist.</p>
+                <a  style="color: white;" href="?p=home">Go Back To Home Page</a>
+            </center>
+        </body>
+</html>
---
+++ view/inc/footer.php
@@ -0,0 +1,48 @@
+ <!-- Footer-->
+ <footer class="bg-white py-4 mt-auto">
+        <section class="bg-light py-5">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-xxl-8">
+                        <div class="text-center my-5">
+                            <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                            <div class="container">
+                                <h2 class="mt-4">Qui suis-je? Ben Salem Rami</h2>
+                                <p>Formé par CF2M en tant que développeur web full-stack.</p>
+                                <p>Passionné de design et de programmation.</p>
+                                
+                              
+                                
+                                <h3 class="mt-4" style="color: rgb(221, 97, 31); margin-bottom: 10px;">Objectifs:</h3>
+                                <ul>
+                                    <p>Devenir un développeur web accompli
+                                    Créer des sites web à la fois esthétiques et fonctionnels
+                                    Collaborer au sein d'une équipe créative
+                                    Progresser professionnellement</p>
+                                </ul>
+                            </div>
+                            <div class="d-flex justify-content-center fs-2 gap-4">
+                                
+                                <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
+                                <a class="text-gradient" target="_blank" href="https://github.com/rammroumm2K"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="container px-5">
+            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                <div class="col-auto">
+                    <div class="small m-0">Copyright &copy; BSR 2024 </div>
+                </div>
+                <div class="col-auto">
+                    <a class="small" href="../">Accueil</a>
+                    <span class="mx-1">&middot;</span>
+                    <a class="small" href="?p=projects">Mes compétences</a>
+                    <span class="mx-1">&middot;</span>
+                    <a class="small" href="?p=resume">Mes travaux réalisés</a>
+                </div>
+            </div>
+        </div>
+    </footer>
